95dabe1c1752d55217c876c07aeffb60932f240e
lkc263 <lkc263@naver.com>
2021-11-26 16:22:36 +0900

    solve the implementation problem, but not complete

--- CodingTestForEmployment/Part3/implementation/implementation5.py
+++ CodingTestForEmployment/Part3/implementation/implementation5.py
@@ -34,11 +34,10 @@ time = 0
 snail_len = 1
 cur_sec = queue_x_c.popleft()
 before_list = []
+tail_data = False
 
 while True:
-    # 만났을 때 종료
-    if array[x][y] == 1:
-        break
+
 
     # 게임 시작, 이동 자리 1로 대체
     array[x][y] = 1
@@ -46,24 +45,34 @@ while True:
     b_right = y
     x += snail_loc[snail_idx][0]
     y += snail_loc[snail_idx][1]
+    time += 1
 
-    print("x: ",x,  " y : ", y)
+    # print("x: ",x,  " y : ", y, " time : ",time)
     if x == 0 or y == 0 or x > n or y > n:
         break
+    # 만났을 때 종료
+    if array[x][y] == 1:
+        break
+
+
+
     # 만약 사과가 있는 위치라면 deque 삽입
     # queue 삽입 알아보기
     if array[x][y] == 2:
-        queue.append([b_left, b_right])
+        if tail_data is False and len(queue) == 0:
+            queue.append([b_left, b_right])
+            tail_data = True
+        queue.append([x, y])
+
     else:
+        tail_data = False
         # 만약 deque가 비어있지 않다면
         if len(queue) > 0:
             before_list = queue.popleft()
             b_left = before_list[0]
             b_right = before_list[1]
-
+            queue.append([x, y])
         array[b_left][b_right] = 0
-    queue.append([x, y])
-    time += 1
 
     if int(cur_sec[0]) == time:
         if cur_sec[1] == 'L':
@@ -73,8 +82,15 @@ while True:
         else:
             snail_idx += 1
             if snail_idx == 4:
-                snail_idx = 1
+                snail_idx = 0
         if len(queue_x_c) > 0:
             cur_sec = queue_x_c.popleft()
+
+
+# print("결과 ")
+# for i in range(1, n+1):
+#     for j in range(1, n+1):
+#         print(array[i][j], end = ' ')
+#     print()
 
 print(time)
