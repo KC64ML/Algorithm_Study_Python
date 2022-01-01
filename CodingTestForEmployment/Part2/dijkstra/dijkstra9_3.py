eb05af984b2498174032ea8f5d69dec5d70336c0
이경창 <lkc263@naver.com>
2022-01-01 22:31:04 +0900

    수정

--- CodingTestForEmployment/Part2/dijkstra/dijkstra9_3.py
+++ CodingTestForEmployment/Part2/dijkstra/dijkstra9_3.py
@@ -3,10 +3,10 @@ import sys
 
 input = sys.stdin.readline
 
-INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
+INF = int(1e9)  # 무한을 의미하는 값으로 10억을 설정
 
 # 노드의 개수, 간선의 개수, 시작 노드를 입력받기
-n, m, start = map(int ,input().split())
+n, m, start = map(int, input().split())
 # 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
 graph = [[] for i in range(n + 1)]
 # 최단 거리 테이블을 모두 무한으로 초기화
@@ -18,12 +18,13 @@ for _ in range(m):
     # a번 노드에서 b번 노드로 가는 비용이 z라는 의미
     graph[x].append((y, z))
 
+
 def dijstra(start):
     q = []
     # 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
     heapq.heappush(q, (0, start))
     distance[start] = 0
-    while q: # 큐가 비어있지 않다면
+    while q:  # 큐가 비어있지 않다면
         # 가장 최단 거리가 짧은 노드에 대한 정보를 꺼내기
         dist, now = heapq.heappop(q)
         if distance[now] < dist:
@@ -37,6 +38,7 @@ def dijstra(start):
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
                 heapq.heappush(q, (cost, i[0]))
+
 
 # 다익스트라 알고리즘을 수행
 dijstra(start)
@@ -52,9 +54,5 @@ for d in distance:
         count += 1
         max_distance = max(max_distance, d)
 
-
 # 시작 노드는 제외해야 하므로 count - 1 출력
 print(count - 1, max_distance)
-
-
-
