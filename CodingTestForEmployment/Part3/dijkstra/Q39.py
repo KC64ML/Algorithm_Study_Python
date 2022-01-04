b836ffb236d3721a79ecf3b89828bd226b5477c7
이경창 <lkc263@naver.com>
2022-01-05 00:26:58 +0900

    📖 dijkstra 완료! (다시 풀어야 한다!)

--- CodingTestForEmployment/Part3/dijkstra/Q39.py
+++ CodingTestForEmployment/Part3/dijkstra/Q39.py
@@ -1,49 +1,50 @@
-from collections import deque
+import heapq
+import sys
 
-t = int(input())
-
-list_array = []
-n = 0
+input = sys.stdin.readline
 INF = int(1e9)
 
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-
-def bfs():
-    array_sum = list_array[0][0]
-    q = deque([])
-    q.append((0, 0, array_sum))
-    # total_data = array_sum
-    total_list = [[INF] * n for _ in range(n)]
-
-    while q:
-        cur_q = q.popleft()
-
-        for idx in range(4):
-            x_cordinate = dx[idx] + cur_q[0]
-            y_cordinate = dy[idx] + cur_q[1]
-
-            if x_cordinate < 0 or x_cordinate >= n or y_cordinate < 0 or y_cordinate >= n:
-                continue
-
-            cur_sum = list_array[x_cordinate][y_cordinate] + cur_q[2]
-
-            if total_list[x_cordinate][y_cordinate] <= cur_sum:
-                continue
-
-            total_list[x_cordinate][y_cordinate] = cur_sum
-
-            q.append((x_cordinate, y_cordinate, cur_sum))
-
-    return total_list[n-1][n-1]
-
-
-for _ in range(t):
+# 전체 테스트 케이스(Test Case) 만큼 반복
+for tc in range(int(input())):
+    # 노드의 개수를 입력받기
     n = int(input())
-    list_array = []
 
-    for _ in range(n):
-        list_array.append(list(map(int, input().split())))
+    # 전체 맵 정보를 입력받기
+    graph = []
 
-    print(bfs())
+    for i in range(n):
+        graph.append(list(map(int, input().split())))
+
+    # 최단 거리 테이블을 모두 무한으로 초기화
+    distance = [[INF] * n for _ in range(n)]
+
+    x, y = 0, 0  # 시작 위치는 (0, 0)
+    # 시작 노드로 가기 위한 비용은 (0, 0) 위치의 값으로 설정하여, 큐에 삽입
+    q = [(graph[x][y], x, y)]
+    distance[x][y] = graph[x][y]
+
+    # 다익스트라 알고리즘 수행
+    while q:
+        # 가장 최단 거리가 짧은 노드에 대한 정보를 꺼내기
+        dist, x, y = heapq.heappop(q)
+        # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
+        if distance[x][y] < dist:
+            continue
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 맵의 범위를 벗어나는 경우 무시
+            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                continue
+
+            cost = dist + graph[nx][ny]
+            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[nx][ny]:
+                distance[nx][ny] = cost
+                heapq.heappush(q, (cost, nx, ny))
+
+    print(distance[n - 1][n - 1])
