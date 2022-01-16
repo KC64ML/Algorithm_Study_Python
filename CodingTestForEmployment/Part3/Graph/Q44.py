65e4e3372e528aff9d8bc5a79cef628936ebd4bb
이경창 <lkc263@naver.com>
2022-01-16 16:30:15 +0900

    👍🏻 Part 3, graph theory completed (need to study again)

--- CodingTestForEmployment/Part3/Graph/Q44.py
+++ CodingTestForEmployment/Part3/Graph/Q44.py
@@ -1,14 +1,15 @@
-
-def find(parent, x):
+# 특정한 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+    # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
     if parent[x] != x:
-        parent[x] = find(parent, parent[x])
-
+        parent[x] = find_parent(parent, parent[x])
     return parent[x]
 
 
-def union(parent, a, b):
-    a = find(parent, a)
-    b = find(parent, b)
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
 
     if a < b:
         parent[b] = a
@@ -16,39 +17,50 @@ def union(parent, a, b):
         parent[a] = b
 
 
+# 노드의 개수 입력받기
 n = int(input())
+parent = [0] * (n + 1)  # 부모 테이블 초기화
 
-list_array = [[0]]
-
+# 모든 간선을 담을 리스트와 최종 비용을 담을 변수
 edges = []
-parent = [0] * (n + 1)
 result = 0
 
+# 부모 테이블 상에서, 부모를 자기 자신으로 초기화
+for i in range(1, n + 1):
+    parent[i] = i
 
+x = []
+y = []
+z = []
 
+# 모든 노드에 대한 좌표 값 입력받기
+for i in range(1, n + 1):
+    data = list(map(int, input().split()))
+    x.append((data[0], i))
+    y.append((data[1], i))
+    z.append((data[2], i))
 
-for idx in range(1, n+1):
-    parent[idx] = idx
+x.sort()
+y.sort()
+z.sort()
 
-for _ in range(n):
-    list_array.append(list(map(int, input().split())))
+# 인접한 노드들로부터 간선 정보를 추출하여 처리
+for i in range(n - 1):
+    # 비용순으로 정렬하기 위해서 튜플의 첫 번째 원소를 비용으로 설정
+    edges.append((x[i + 1][0] - x[i][0], x[i][1], x[i+1][1]))
+    edges.append((y[i + 1][0] - y[i][0], y[i][1], y[i+1][1]))
+    edges.append((z[i + 1][0] - z[i][0], z[i][1], z[i+1][1]))
 
-for i in range(1, n):
-    list_in_array = []
-
-    for j in range(i + 1, n + 1):
-        cost = min(abs(list_array[i][0] - list_array[j][0])
-                       , abs(list_array[i][1] - list_array[j][1]),
-                       abs(list_array[i][2] - list_array[j][2]))
-        edges.append((cost, i, j))
-
+# 간선을 비용순으로 정렬
 edges.sort()
 
+# 간선을 하나씩 확인하며
 for edge in edges:
     cost, a, b = edge
-
-    if find(parent, a) != find(parent, b):
-        union(parent, a, b)
+    # 사이클이 발생하지 않는 경우에만 집합에 포함
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
         result += cost
+
 
 print(result)
