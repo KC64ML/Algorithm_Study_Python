deb02963a6f7ab6808529aec8a757832342bac57
이경창 <lkc263@naver.com>
2022-01-28 00:04:37 +0900

    😨 finish 2156, next time 11053 can't focus, dp problem

--- RTplzrunBlog/DP/2156.py
+++ RTplzrunBlog/DP/2156.py
@@ -7,16 +7,23 @@ arr = [0]
 for _ in range(n):
     arr.append(int(input()))
 
+# ① 현재 포도주와 이전 포도주를 마시고 전전 포두주는 마시지 않는다. ( wine[i]+wine[i-1]+d[i-3] )
+# ② 현재 포도주와 전전 포도주를 마시고 이전 포도주는 마시지 않는다. ( wine[i]+d[i-2] )
+# ③ 현재 포도주를 마시지 않는다. ( d[i-1] )
+
+# d[i-1]에 해당 케이스를 포함한 최댓값이 저장되어 있다.
+
+
 dp[1] = arr[1]
 
-for idx in range(2, n + 1):
-    if idx == 2:
-        dp[idx] = arr[1] + arr[2]
-    else:
-        dp[idx] = max(dp[idx - 2] + arr[idx], dp[idx - 1] + arr[idx], dp[idx - 2] + dp[idx - 1])
+if n >= 2:
+    dp[2] = arr[1] + arr[2]
 
-print(dp)
+    for idx in range(3, n + 1):
+        dp[idx] = max(dp[idx-1], dp[idx - 2] + arr[idx], dp[idx - 3] + arr[idx - 1] + arr[idx])
 
-# https://pacific-ocean.tistory.com/152 참고하기
+print(dp[n])
+
+# 참고 : https://hongcoding.tistory.com/48
 
 
