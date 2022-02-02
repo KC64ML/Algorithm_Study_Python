7785f5d70920d53bd675652bda33baba8a96d2b9
이경창 <lkc263@naver.com>
2022-02-03 01:02:36 +0900

    😁 2133 complete!

--- RTplzrunBlog/DP/1699.py
+++ RTplzrunBlog/DP/1699.py
@@ -1,18 +1,18 @@
-n = int(input())
-
-dp = []
-
-for idx in range(n + 1):
-    dp.append(idx)
-
-for i in range(2, n+ 1):
-    for j in range(2, i+1):
-        if j * j > i:
-            break
-        else:
-            dp[i] = min(dp[i], dp[i - j * j] + 1)
-
-print(dp[n])
+# n = int(input())
+#
+# dp = []
+#
+# for idx in range(n + 1):
+#     dp.append(idx)
+#
+# for i in range(2, n+ 1):
+#     for j in range(2, i+1):
+#         if j * j > i:
+#             break
+#         else:
+#             dp[i] = min(dp[i], dp[i - j * j] + 1)
+#
+# print(dp[n])
 
 
 # 참고하여 수정한 코드
