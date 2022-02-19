8ca005c83fbe364713858b96988fa7796aa1cb38
이경창 <lkc263@naver.com>
2022-02-20 01:31:12 +0900

    📣 2331 completed!

--- RTplzrunBlog/Graph/2331.py
+++ RTplzrunBlog/Graph/2331.py
@@ -1,55 +1,19 @@
 from sys import stdin as s
-from collections import deque
+
+a, p = map(int, s.readline().split())
 
 
-def dfs(a, p):
-    cur_data = 0
-    for idx in range(len(a)):
-        cur_data += pow(int(a[idx]), p)
+cur_value = [a]
 
-    if visited[cur_data]:
-        return cur_data
+while True:
+    cnt = 0
 
-    visited[cur_data] = True
-    queue.append(cur_data)
-    return dfs(str(cur_data), p)
+    for cur_data in str(cur_value[-1]):
+        cnt += int(cur_data) ** p
 
-
-a, p = s.readline().split()
-p = int(p)
-
-visited = [False] * 1000000
-queue = deque()
-
-loop_start_point = dfs(a, p)
-# print(queue)
-result = 0
-
-while queue:
-    cur_data = queue.popleft()
-    if cur_data == loop_start_point:
+    if cnt in cur_value:
         break
-    result += 1
-    # print(result)
 
-if result != 0:
-    result += 1
-print(result)
+    cur_value.append(cnt)
 
-# import sys
-# a, p = map(int, sys.stdin.readline().split())
-# seq = [a]
-# while True:
-#     t = seq[-1]
-#     val = 0
-#     while t:
-#         val += ((t%10) ** p)
-#         t //= 10
-#     if val in seq:
-#         sys.stdout.write(str(seq.index(val)))
-#         break
-#     else:
-#         seq.append(val)
-
-
-# 참고 https://haesoo9410.tistory.com/189
+print(cur_value.index(cnt))
