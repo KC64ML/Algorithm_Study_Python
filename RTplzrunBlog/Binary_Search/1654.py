cf3fb1c1779691e212ce315199f5d9b5306c659b
이경창 <lkc263@naver.com>
2022-03-09 00:38:44 +0900

    ✏️ 1654 complete tomorrow!

---
+++ RTplzrunBlog/Binary_Search/1654.py
@@ -0,0 +1,19 @@
+import sys
+K, N = map(int, input().split())
+lan = [int(sys.stdin.readline()) for _ in range(K)]
+start, end = 1, max(lan) #이분탐색 처음과 끝위치
+
+
+def quick_sort(array):
+    # 리스트가 하나 이하의 원소만을 담고 있다면 종료
+    if len(array) <= 1:
+        return array
+
+    pivot = array[0]  # 피벗은 첫 번째 원소
+    tail = array[1:]  # 피벗을 제외한 리스트
+
+    left_side = [x for x in tail if x <= pivot]  # 분할된 왼쪽 부분
+    right_side = [x for x in tail if x > pivot]  # 분할된 오른쪽 부분
+
+    # 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬을 수행하고, 전체 리스트를 반환
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
