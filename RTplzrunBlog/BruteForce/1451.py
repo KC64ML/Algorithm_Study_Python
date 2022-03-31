3b900153d67762e75f8908097f604869de8f0dd0
이경창 <lkc263@naver.com>
2022-03-31 18:49:17 +0900

    ✏️ 1451 completed! brute_force

--- RTplzrunBlog/BruteForce/1451.py
+++ RTplzrunBlog/BruteForce/1451.py
@@ -4,7 +4,7 @@ read = sys.stdin.readline
 
 n, m = map(int, read().split())
 
-arr = [[0] * (m+1)]
+arr = [[0] * (m + 1)]
 
 for i in range(n):
     arr.append([0] + list(map(int, read().strip())))
@@ -25,21 +25,18 @@ def sum_of_rec(x1, y1, x2, y2):
 
 result = 0
 ans = 0
-print(sum_rec)
 
-# (1)
-for i in range(1, n):
-    for j in range(1, m):
-        num1 = sum_of_rec(1, 1, i, j)
-        num2 = sum_of_rec(1, j + 1, i, m)
-        num3 = sum_of_rec(i + 1, 1, n, m)
-
-        # print(num1+num2+num3)
+# (1) 전체 직사각형을 세로로만 분할한 경우
+for i in range(1, m - 1):
+    for j in range(i + 1, m):
+        num1 = sum_of_rec(1, 1, n, i)
+        num2 = sum_of_rec(1, i + 1, n, j)
+        num3 = sum_of_rec(1, j + 1, n, m)
 
         if result < num1 * num2 * num3:
             result = num1 * num2 * num3
 
-# (2)
+# (2) 전체 직사각형을 가로로만 분할한 경우
 for i in range(1, n - 1):
     for j in range(i + 1, n):
         num1 = sum_of_rec(1, 1, i, m)
@@ -49,107 +46,44 @@ for i in range(1, n - 1):
         if result < num1 * num2 * num3:
             result = num1 * num2 * num3
 
-# (3)
+# (3) 전체 세로 분할 후, 우측 가로 분할한 경우
+for i in range(1, m):
+    for j in range(1, n):
+        num1 = sum_of_rec(1, 1, n, i)
+        num2 = sum_of_rec(1, i + 1, j, m)
+        num3 = sum_of_rec(j + 1, i + 1, n, m)
+
+        if result < num1 * num2 * num3:
+            result = num1 * num2 * num3
+
+# (4) 전체 세로 분할 후, 좌측 가로 분할한 경우
+for i in range(1, n):
+    for j in range(1, m):
+        num1 = sum_of_rec(1, 1, i, j)
+        num2 = sum_of_rec(i + 1, 1, n, j)
+        num3 = sum_of_rec(1, j + 1, n, m)
+
+        if result < num1 * num2 * num3:
+            result = num1 * num2 * num3
+
+# (5) 전체 가로 분할 후, 하단 세로 분할한 경우
 for i in range(1, n):
     for j in range(1, m):
         num1 = sum_of_rec(1, 1, i, m)
         num2 = sum_of_rec(i + 1, 1, n, j)
         num3 = sum_of_rec(i + 1, j + 1, n, m)
 
-        # print(num1 + num2 + num3)
-
         if result < num1 * num2 * num3:
             result = num1 * num2 * num3
 
-# (4)
-for i in range(1, m - 1):
-    for j in range(i + 1, m):
-        num1 = sum_of_rec(1, 1, n, i)
-        num2 = sum_of_rec(1, i + 1, n, j)
-        num3 = sum_of_rec(1, j + 1, n, m)
-
-        # print(num1 + num2 + num3)
-
-        if result < num1 * num2 * num3:
-            result = num1 * num2 * num3
-
-# (5)
-for i in range(1, n):
-    for j in range(1, m):
-        num1 = sum_of_rec(1, 1, n, i)
-        num2 = sum_of_rec(1, i+1, n, j)
-        num3 = sum_of_rec(i, j+1, n, m)
-
-        if result < num1 * num2 * num3:
-            result = num1 * num2 * num3
-
-# (6)
+# (6) 전체 가로 분할 후, 상단 세로 분할한 경우
 for i in range(1, n):
     for j in range(1, m):
         num1 = sum_of_rec(1, 1, i, j)
-        num2 = sum_of_rec(i + 1, 1, n, j)
-        num3 = sum_of_rec(i, j + 1, n, m)
+        num2 = sum_of_rec(1, j + 1, i, m)
+        num3 = sum_of_rec(i + 1, 1, n, m)
 
         if result < num1 * num2 * num3:
             result = num1 * num2 * num3
 
-
-# 첫 번째 경우: 전체 직사각형을 세로로만 분할한 경우
-# for i in range(1, m - 1):
-#     for j in range(i + 1, m):
-#         r1 = sum_cal(1, 1, n, i)
-#         r2 = sum_cal(1, i + 1, n, j)
-#         r3 = sum_cal(1, j + 1, n, m)
-#         if ans < r1 * r2 * r3:
-#             ans = r1 * r2 * r3
-#
-# # 두 번째 경우: 전체 직사각형을 가로로만 분할한 경우
-# for i in range(1, n - 1):
-#     for j in range(i + 1, n):
-#         r1 = sum_cal(1, 1, i, m)
-#         r2 = sum_cal(i + 1, 1, j, m)
-#         r3 = sum_cal(j + 1, 1, n, m)
-#         if ans < r1 * r2 * r3:
-#             ans = r1 * r2 * r3
-#
-# # 세 번째 경우: 전체 세로 분할 후 우측 가로 분할한 경우
-# for i in range(1, m):
-#     for j in range(1, n):
-#         r1 = sum_cal(1, 1, n, i)
-#         r2 = sum_cal(1, i + 1, j, m)
-#         r3 = sum_cal(j + 1, i + 1, n, m)
-#         if ans < r1 * r2 * r3:
-#             ans = r1 * r2 * r3
-#
-# # 네 번째 경우: 전체 세로 분할 후 좌측 가로 분할한 경우
-# for i in range(1, n):
-#     for j in range(1, m):
-#         r1 = sum_cal(1, 1, i, j)
-#         r2 = sum_cal(i + 1, 1, n, j)
-#         r3 = sum_cal(1, j + 1, n, m)
-#         if ans < r1 * r2 * r3:
-#             ans = r1 * r2 * r3
-#
-# # 다섯번 째 경우: 전체 가로 분할 후 하단 세로 분할한 경우
-# for i in range(1, n):
-#     for j in range(1, m):
-#         r1 = sum_cal(1, 1, i, m)
-#         r2 = sum_cal(i + 1, 1, n, j)
-#         r3 = sum_cal(i + 1, j + 1, n, m)
-#         if ans < r1 * r2 * r3:
-#             ans = r1 * r2 * r3
-#
-# # 여섯번 째 경우: 전체 가로 분할 후 상단 세로 분할한 경우
-# for i in range(1, n):
-#     for j in range(1, m):
-#         r1 = sum_cal(1, 1, i, j)
-#         r2 = sum_cal(1, j + 1, i, m)
-#         r3 = sum_cal(i + 1, 1, n, m)
-#         if ans < r1 * r2 * r3:
-#             ans = r1 * r2 * r3
-
 print(result)
-# print(ans)
-
-
-# 가로 세로 어떻게 나눈건지 다시 알아보기
