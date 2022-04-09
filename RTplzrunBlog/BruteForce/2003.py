97f2017e4d6227ba700f4742d83dfe20b62e3779
이경창 <lkc263@naver.com>
2022-04-09 12:48:27 +0900

    ✏️ 2003 completed! (brute force algorithm)

--- RTplzrunBlog/BruteForce/2003.py
+++ RTplzrunBlog/BruteForce/2003.py
@@ -3,20 +3,34 @@ import sys
 read = sys.stdin.readline
 
 n, m = map(int, read().split())
-
 arr = list(map(int, read().split()))
 
-cnt = 0
+arr = arr + [0] * (10001 - len(arr))
 
-for i in range(n):
-    if arr[i] > m:
-        continue
-    for j in range(i, n):
-        cur_result = sum(arr[i:j+1])
 
-        if cur_result > m:
-            break
-        elif cur_result == m:
-            cnt += 1
+start = 0
+end = 0
+partition = 0
 
-print(cnt)
+result = 0
+
+# end <= n인 이유 (원래 마지막 인덱스가 n - 1이다.)
+# end가 마지막 인덱스에 도착하고 다음으로 넘어갈 때, 더이상 갈곳이 없어
+# start를 돌려줘야 한다.
+
+
+while end <= n:
+    # 현재 원하는 m보다 작을 경우, end를 증가
+    # 햔제 원하는 m과 같거나 클 경우, start를 증가
+    if partition < m:
+        partition += arr[end]
+        end += 1
+    elif partition >= m:
+        partition -= arr[start]
+        start += 1
+
+    # 도착했다면 횟수를 증가한다.
+    if partition == m:
+        result += 1
+
+print(result)
