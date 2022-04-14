06b864d583f7e45f26a9f5fd672857f23009cdfa
이경창 <lkc263@naver.com>
2022-04-15 01:05:34 +0900

    ✏️ 2143, 2632 completed! (brute force, final problem)

--- RTplzrunBlog/BruteForce/2143.py
+++ RTplzrunBlog/BruteForce/2143.py
@@ -9,17 +9,21 @@ a = list(map(int, read().split()))
 m = int(read())
 b = list(map(int, read().split()))
 
-cnt = 0
+answer = 0
 
 dist = dict()
 
+for i in range(n):
+    cur_sum = 0
+    for j in range(i, n):
+        cur_sum += a[j]
+        dist[cur_sum] = dist.get(cur_sum, 0) + 1
 
-for i in range(len(a)):
-    for j in range(i+1, len(a)+1):
-        dist[sum(a[i:j])] = dist.get(sum(a[i:j]), 0) + 1
+for i in range(m):
+    cur_sum = 0
+    for j in range(i, m):
+        cur_sum += b[j]
+        answer += dist.get(t-cur_sum, 0)
 
-for i in range(len(b)):
-    for j in range(i+1, len(b)+1):
-        cnt += dist.get(t-sum(b[i:j]), 0)
+print(answer)
 
-print(cnt)
