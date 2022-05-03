b46bc3f2fabf89b73c9972b42a5bfc008bb10dfa
이경창 <lkc263@naver.com>
2022-05-04 00:00:15 +0900

    ✏️ Lv2 전화번호 목록 Completed

---
+++ Programmers/Lv2/42577.py
@@ -0,0 +1,10 @@
+def solution(phoneBook):
+    answer = True
+    phoneBook = sorted(phoneBook)
+
+    for p1, p2 in zip(phoneBook, phoneBook[1:]):
+        if p2.startswith(p1):
+            answer = False
+            break
+
+    return answer
