d896d523c0d5181d9845430dd237fb98bcad9209
이경창 <lkc263@naver.com>
2022-05-14 13:10:15 +0900

    ✏️ 1114 completed (greedy, binary tree)

--- RTplzrunBlog(baekjoon)/GreedyStudy/1114.py
+++ RTplzrunBlog(baekjoon)/GreedyStudy/1114.py
@@ -13,16 +13,16 @@ def solution(x):
     prev = []
     first = 0
 
-    # x라는 길이를 넘으면 자른다.  
+    # x라는 길이를 넘으면 자른다.
     for i in range(len(cuts) - 1, -1, -1):
         diff = cuts[i] - cuts[i - 1]
         total = cut_start - cuts[i]
         print("i : ", i, " cur_start", cut_start, "cuts : ", cuts, "total : ", total, " x: ", x, " diff : ", diff)
         if diff > x:
-            # 이분탐색, 왼쪽  
+            # 이분탐색, 오른쪽
             return 10001, 0
         elif total > x:
-            # i번째 자르고, i+1을 저장  
+            # i번째 자르고, i+1을 저장
             cut_start = cuts[i + 1]
             prev.append(cuts[i + 1])
             cnt += 1
@@ -32,7 +32,7 @@ def solution(x):
     if cnt < C:
         first = cuts[1]
     else:
-        # 인덱스 뒤에서부터 삽입되니, 마지막으로 추가된 것이 first 값이 된다.  
+        # 인덱스 뒤에서부터 삽입되니, 마지막으로 추가된 것이 first 값이 된다.
         first = prev[-1]
 
     return cnt, first
