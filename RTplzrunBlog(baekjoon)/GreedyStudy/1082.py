5870c60267d1362c2fcb2a7f117c59802c707d60
이경창 <lkc263@naver.com>
2022-05-24 11:02:21 +0900

    ✏️ 1082 - 방번호

--- RTplzrunBlog(baekjoon)/GreedyStudy/1082.py
+++ RTplzrunBlog(baekjoon)/GreedyStudy/1082.py
@@ -15,6 +15,9 @@ dp = [-sys.maxsize] * (m + 1)
 for i in range(n - 1, -1, -1):
     x = room[i]
     for j in range(x, m + 1):
+        # dp[j - x] * 10, 이전에 구한 dp 값 [j-x] * 10
+        # i는 방번호
+        # dp[j] : 이전에 구한 dp 값
         dp[j] = max(dp[j - x] * 10 + i, i, dp[j])
 
 print(dp[m])
