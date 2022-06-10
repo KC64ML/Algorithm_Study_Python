58b2d488d37831392bccc8de251f56c7d732bf21
lkc263 <lkc263@naver.com>
2022-06-10 20:57:49 +0900

    ✏ 2212 - greedy

--- RTplzrunBlog(baekjoon)/GreedyStudy/2212.py
+++ RTplzrunBlog(baekjoon)/GreedyStudy/2212.py
@@ -6,18 +6,16 @@ n = int(read())
 k = int(read())
 
 sensor = list(map(int, read().split()))
+s = []
 
 sensor.sort()
 
-minusSection = []
+for i in range(1, n):
+    s.append(abs(sensor[i] - sensor[i-1]))
+
+s.sort(reverse=True)
 answer = 0
 
-for i in range(n-1):
-    minusSection.append(abs(sensor[i] - sensor[i+1]))
-
-
-minusSection.sort(reverse=True)
 for i in range(k-1, n-1):
-    answer += minusSection[i]
-
+    answer += s[i]
 print(answer)
