900aead774d77f2b05309f237fe35307cde3bfbf
lkc263 <lkc263@naver.com>
2022-06-11 17:12:56 +0900

    ✏ 2141 greedy

--- RTplzrunBlog(baekjoon)/GreedyStudy/2141.py
+++ RTplzrunBlog(baekjoon)/GreedyStudy/2141.py
@@ -4,34 +4,36 @@ read = sys.stdin.readline
 
 n = int(read())
 
-village = []
+arr = []
 
-for _ in range(n):
+for i in range(n):
     a, b = map(int, read().split())
-    village.append((a, b))
+    arr.append([a, b])
 
-village.sort()
-
-start = 0
-end = n
+arr.sort()
 
 answer = float(1e9)
+start = 0
+end = n
 
 while start <= end:
     mid = (start + end) // 2
     left = 0
     right = 0
 
-    for i in range(0, mid+1):
-        left += village[i][1]
+    for i in range(mid+1):
+        left += arr[i][1]
 
-    for i in range(mid + 1, n):
-        right += village[i][1]
+    for i in range(mid+1, n):
+        right += arr[i][1]
 
     if left < right:
         start = mid + 1
     else:
         end = mid - 1
-        answer = min(answer, village[mid][0])
+        answer = min(answer, arr[mid][0])
 
 print(answer)
+
+
+
